a623366d9f0f37f6b9f246d57b9efb4a98650c4a
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-08 21:16:52 +0530

    slug  url

--- Django/mywebsite/blogs/urls.py
+++ Django/mywebsite/blogs/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home_page),
     path('allposts', views.blogposts),
-    path("allposts/<int:blog>", views.blog_post_by_number),
-    path("allposts/<str:blog>", views.blog_post),
+    path("allposts/<slug:blog>", views.blog_post),
 ]
