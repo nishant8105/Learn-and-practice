36d2f28c309a952ec431b7e59a42012c67375727
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-15 22:14:06 +0530

    decision tree model

---
+++ Machine_learning/model_Practice/decision.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv('clean_titanic.csv')
+
+X = df.drop("survived", axis=1)
+y = df["survived"]
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.20, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(X_train_scaled,y_train)
+
+y_pred = dt.predict(X_test_scaled)
+
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+classifier = classification_report(y_test, y_pred)
+
+print(accuracy)
+print(confusion)
+print(classifier)
